d33c9f7e814dcfad6b5858eda228218af3fa9167
Ewemor Martin <martinewemor@gmail.com>
2024-04-11 16:10:25 +0200

    oraiMunka

---
+++ adat.js
@@ -0,0 +1,40 @@
+export const ADATLISTA=[
+
+    {
+        nev:"Éva",
+        kor:12,
+        nem:true
+    },
+    {
+        nev:"Martin",
+        kor:42,
+        nem:false
+    },
+    {
+        nev:"Sándor",
+        kor:78,
+        nem:false
+    },
+    {
+        nev:"István",
+        kor:72,
+        nem:false
+    },
+    {
+        nev:"Ádám",
+        kor:62,
+        nem:false
+    },
+    {
+        nev:"Bálint",
+        kor:82,
+        nem:false
+    },
+    {
+        nev:"Dóra",
+        kor:32,
+        nem:true
+    },
+    
+   
+]
---
+++ main.js
@@ -0,0 +1,4 @@
+import { ADATLISTA } from "./adat.js";
+import { megjelenit, tablazatLetrehoz } from "./fuggvenyek.js";
+
+megjelenit(tablazatLetrehoz(ADATLISTA))
